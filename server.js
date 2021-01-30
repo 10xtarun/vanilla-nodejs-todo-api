42406f179b4045dc2cafe0587e8c7f49b58f9115
10xtarun <10xtarun@gmail.com>
2021-01-30 11:51:17 +0530

    added get request

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 // import core http module of nodejs
 const http = require("http")
+const url = require("url")
+const qs = require("querystring")
+
+let TODO = [
+    {
+        name: "Task 1",
+        completed: false
+    },
+    {
+        name: "Task 2",
+        completed: false
+    },
+    {
+        name: "Task 3",
+        completed: false
+    }
+];
 
 //create basic server 
 /**
@@ -7,8 +24,24 @@ const http = require("http")
  * @param {Object} res : response
  */
 const server = http.createServer(function (req, res) {
-    res.end("Hello World!!!")
+    if(req.method === 'GET') {
+        return getTodos(req, res)
+    }
 })
+
+function getTodos(req, res) {
+    const req_url = url.parse(req.url);
+    if(req_url.pathname != '/todos') {
+        return handleError(res, 404)
+    } 
+    res.setHeader('Content-Type', 'application/json;charset=utf-8')
+    return res.end(JSON.stringify(TODO));
+}
+
+function handleError(res, code) {
+    res.statusCode = code
+    res.end(`404 Not Found!!!`)
+}
 
 //make server run on some PORT 
 /** here we select port number 3000 */
